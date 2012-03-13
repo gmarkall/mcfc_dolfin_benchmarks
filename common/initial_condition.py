79e9abb0058d8c53861d3a4e8505de359fd52874
Graham Markall <grm08@doc.ic.ac.uk>
2012-03-13 15:35:41 +0000

    Add initial condition

---
+++ common/initial_condition.py
@@ -0,0 +1,9 @@
+def val(X, t):
+    x = X[0]+0.5
+    y = X[1]
+    r = sqrt(x*x + y*y)
+    if r<0.25:
+        return A*(exp((-r**2)/(4*D*t))/(4*pi*D*t))
+    else:
+        return 0.0
+
---
+++ dolfin/initial_condition.py
@@ -0,0 +1,1 @@
+../common/initial_condition.py
