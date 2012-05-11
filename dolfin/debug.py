0320e495b1e951c55cca9d98e0c4019ad9d8e9ba
gmarkall <grm08@doc.ic.ac.uk>
2012-05-11 18:44:00 +0100

    Simplify debug.py

--- dolfin/debug.py
+++ dolfin/debug.py
@@ -1,10 +1,25 @@
 from dolfin import *
-from initial_condition import val
-from parameters import *
+from math import sqrt, exp, pi
 
-#parameters["num_threads"] = 6
-#parameters["form_compiler"]["cpp_optimize"] = True
-#parameters["form_compiler"]["optimize"] = True
+# Parameters
+
+D = 0.1       # Diffusivity
+A = 0.1       # Normalisation
+t = 0.1       # Starting time
+dt = 0.00015625  # Timestep
+endtime = 0.2 # Simulation finish time
+
+# Initial condition
+
+def val(X, t):
+    x = X[0]-0.5
+    y = X[1]-0.5
+    r = sqrt(x*x + y*y)
+    d = 0.05
+    if r<0.25:
+        return A*(exp((-r**2)/(4*d*t))/(4*pi*d*t))
+    else:
+        return 0.0
 
 class InitialCondition(Expression):
     def __init__(self, fn):
@@ -15,31 +30,22 @@ class InitialCondition(Expression):
 
 def simulation(D, A, t, dt, endtime, mesh, initial):
 
-    # Added due to mesh not conforming to UFC numbering, allegedly
+    # Added due to mesh not conforming to UFC numbering
     mesh.order()
 
     # Create FunctionSpaces
     T = FunctionSpace(mesh, "CG", 1)
     V = VectorFunctionSpace(mesh, "CG", 1)
 
-    # Create velocity Function
-    velocity = Constant( (5.0, 0.0) )
-
-    concentration = InitialCondition(initial)
 
     # Initialise source function and previous solution function
     u0 = Function(T)
     u1 = Function(T)
+    concentration = InitialCondition(initial)
     u1.assign(concentration)
-
-    # Test and trial functions
     u, v = TrialFunction(T), TestFunction(T)
 
-    # Advection
     Mass = v*u*dx
-    adv_rhs = (v*u0+dt*dot(grad(v),velocity)*u0)*dx
-
-    #Diffusion
     d = -dt*D*dot(grad(v),grad(u))*dx
     diff_matrix = Mass - 0.5*d
     diff_rhs = action(Mass + 0.5*d, u0)
@@ -47,64 +53,17 @@ def simulation(D, A, t, dt, endtime, mesh, initial):
     out_file = File("temperature.pvd")
     out_file << (u1, t)
 
-    sp = { "linear_solver":  "cg", "preconditioner": "jacobi" }
     fcp = { "optimize": True }
-
-    adv = False
-    diff = True
-
-    t_a_a = Timer("advection assembly")
-    t_a_s = Timer("advection solve")
-    t_d_a = Timer("diffusion assembly")
-    t_d_s = Timer("diffusion solve")
-
-    t_loop = Timer("Timestepping loop")
 
     # Time-stepping
     while t < endtime:
-
-        # Copy soln from prev.
         u0.assign(u1)
         print "Max:", u0.vector().max()
-
-        if adv:
-            # Advection
-            t_a_a.start()
-            #M = assemble(Mass)
-            #b = assemble(adv_rhs)
-            t_a_a.stop()
-            t_a_s.start()
-            solve(Mass==adv_rhs, u1, solver_parameters=sp, form_compiler_parameters=fcp)
-            t_a_s.stop()
-
-            # Copy solution from advection
-            u0.assign(u1)
-
-        if diff:
-            # Diffusion
-            t_d_a.start()
-            #M = assemble(diff_matrix)
-            #b = assemble(diff_rhs)
-            t_d_a.stop()
-            t_d_s.start()
-            solve(diff_matrix==diff_rhs, u1, solver_parameters=sp, form_compiler_parameters=fcp)
-            t_d_s.stop()
-        #if save_output:
-        #    out_file << (u1, t)
-     
-        # Next timestep
+        solve(diff_matrix==diff_rhs, u1, form_compiler_parameters=fcp)
         t += dt
 
-    t_loop.stop()
-
-    list_timings()
-    
     out_file << (u1, t)
 
-    return t_loop.value()
-
-
-# Load mesh
-mesh = Mesh("../mesh/cdisk.xml")
+mesh = UnitSquare(32, 32)
 
 simulation(D, A, t, dt, endtime, mesh, val)
