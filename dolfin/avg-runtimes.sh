a712e51d643cd9d80848de9ac635cf9806769bf3
gmarkall <grm08@doc.ic.ac.uk>
2012-05-30 17:56:34 +0100

    Scripts and plotting changes and adds

--- dolfin/avg-runtimes.sh
+++ dolfin/avg-runtimes.sh
@@ -4,7 +4,7 @@ rm -f stats/times.txt
 
 echo -e "Cores\tTime" > stats/times.txt
 
-for i in `seq 1 11`; do
+for i in `seq 1 12`; do
 {
   a=`cat stats/output$i-run*.txt | grep Timestepping | awk 'BEGIN { s=0 } {s=s+$5} END {print s/NR}'`;
   echo -e "$i\t$a" >> stats/times.txt;
